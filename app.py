09bcd4a73184c7dbc047e151001c2d3a0f7f173e
drkalai1992 <52269819+drkalai1992@users.noreply.github.com>
2023-02-28 22:35:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    print('Kalai')
+def hello():
+    from torrentp import TorrentDownloader
+    for i in range(1):
+        torrent_file = TorrentDownloader(input(), '.')
+        torrent_file.start_download()
+hello()
