4dbc50802640efdbaef2a00799314586afc325a1
drkalai1992 <52269819+drkalai1992@users.noreply.github.com>
2023-02-28 22:52:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 def hello():
     from torrentp import TorrentDownloader
     for i in range(1):
-        torrent_file = TorrentDownloader(input(), '.')
+        torrent_file = TorrentDownloader(input())
         torrent_file.start_download()
 hello()
